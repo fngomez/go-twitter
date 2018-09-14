2c27197820401ae0996db2b1c0aad147132d4417
FERNANDO GOMEZ <fegomez@ar0fvfwr134hv2j.ml.com>
2018-09-14 16:08:13 -0300

    ejercicio 4

--- service/tweet_manager.go
+++ service/tweet_manager.go
@@ -3,14 +3,30 @@ package service
 import (
 	"github.com/fngomez/go-twitter/domain"
 	"time"
+	"errors"
 )
 
 var _tweet *domain.Tweet;
 
-func PublishTweet(tweet *domain.Tweet) {
+func PublishTweet(tweet *domain.Tweet) error {
+
+	if tweet.User == "" {
+		return errors.New("user is required")
+	}
+
+	if len(tweet.Text) == 0 {
+		return errors.New("text is required")
+	}
+
+	if len(tweet.Text) > 140 {
+		return errors.New("text exceeds 140 characters")
+	}
+
 	_tweet = tweet
 	aux := time.Now()
 	_tweet.Date = &aux
+
+	return nil
 }
 
 func GetTweet() *domain.Tweet {
